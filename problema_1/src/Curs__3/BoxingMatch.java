05ed43fcff13d032b144524a64d82000d2244149
RobertPopirlan <116944952+RobertPopirlan@users.noreply.github.com>
2022-11-21 21:12:21 +0200

    Add files via upload

--- problema_1/src/Curs__3/BoxingMatch.java
+++ problema_1/src/Curs__3/BoxingMatch.java
@@ -1,2 +1,27 @@
-package Curs__3;public class BoxingMatch {
-}
+package Curs__3;
+
+public class BoxingMatch {
+    private fightWin f1;
+    private fightWin f2;
+
+    public BoxingMatch(fightWin f1, fightWin f2) {
+        this.f1 = f1;
+        this.f2 = f2;
+    }
+
+
+    public String fightWin() {
+        while (f1.getHealth() > 0 && f2.getHealth() > 0) {
+            f1.attack(f2);
+            if (f2.getHealth() <= 0)
+                return f1.toString();
+                {
+                    f2.attack(f1);
+                }
+
+            }
+            return f2.toString();
+        }
+
+    }
+
--- problema_1/src/Curs__3/Main.java
+++ problema_1/src/Curs__3/Main.java
@@ -1,2 +1,11 @@
-package Curs__3;public class Main {
+package Curs__3;
+
+public class Main {
+    public static void main(String[] args) {
+        fightWin f1 = new fightWin("f1", 100, 10);
+        fightWin f2 = new fightWin("f2", 100, 10);
+        BoxingMatch BoxingMatch = new BoxingMatch(f1, f2);
+        String winner = BoxingMatch.fightWin();
+        System.out.println(winner);
+    }
 }
